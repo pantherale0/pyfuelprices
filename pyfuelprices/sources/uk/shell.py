4f0b1cde501720dc899cf3d07ce9fcc20f0577fa
Jordan <jordan@hrvy.uk>
2024-01-03 00:52:01 +0000

    2.0.1
    resolve issue with shell datasource

--- pyfuelprices/sources/uk/shell.py
+++ pyfuelprices/sources/uk/shell.py
@@ -20,7 +20,7 @@ class ShellUKSource(CMAParser):
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0"
     }
 
-    async def update(self, _=None) -> list[FuelLocation]:
+    async def update(self, areas=None) -> list[FuelLocation]:
         """Update hooks for the data source."""
         if datetime.now() > self.next_update:
             _LOGGER.debug("Starting update hook for %s to url %s", self.provider_name, self._url)
