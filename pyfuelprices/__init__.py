349d16958ccb14c9e6d96e019ee5784394074ebb
Jordan <jordan@hrvy.uk>
2025-03-17 11:29:44 +0000

    add get_source_config_schema function

--- pyfuelprices/__init__.py
+++ pyfuelprices/__init__.py
@@ -118,6 +118,13 @@ class FuelPrices:
         return sorted(fuels, key=lambda item: item["cost"])
 
     @staticmethod
+    def get_source_config_schema(source_shortcode: str):
+        """Return the config schema for a given source."""
+        if source_shortcode not in SOURCE_MAP:
+            raise ValueError(f"Source {source_shortcode} not found")
+        return SOURCE_MAP[source_shortcode][0].attr_config
+
+    @staticmethod
     def source_config_type(source_shortcode: str) -> SupportsConfigType:
         """Return the config type of a given source."""
         if source_shortcode not in SOURCE_MAP:
