dd2516c84dd50d68c38271cd07229c0df0edfa06
Jordan <jordan@hrvy.uk>
2025-03-20 12:42:41 +0000

    add missing area schema

--- pyfuelprices/schemas.py
+++ pyfuelprices/schemas.py
@@ -2,16 +2,20 @@
 
 import voluptuous as vol
 
+from .const import PROP_AREA_LONG, PROP_AREA_LAT, PROP_AREA_RADIUS
+
 SOURCE_BASE_CONFIG = vol.Schema({}, extra=vol.ALLOW_EXTRA)
 AREA_CONFIG = vol.Schema({
-    vol.All()
+    vol.Inclusive(PROP_AREA_LAT, "loc"): float,
+    vol.Inclusive(PROP_AREA_LONG, "loc"): float,
+    vol.Inclusive(PROP_AREA_RADIUS, "loc"): float
 })
 
 BASE_CONFIG_SCHEMA = vol.Schema(
     {
         vol.Optional("global"): SOURCE_BASE_CONFIG,
         vol.Required("providers",default={}): SOURCE_BASE_CONFIG,
-        vol.Required("areas", default=[]): list,
+        vol.Required("areas", default=[]): list[AREA_CONFIG],
         vol.Optional("country_code", default=""): str,
         vol.Required("update_interval", default=24): int,
         vol.Required("timeout", default=30): int
