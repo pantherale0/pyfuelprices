96df1d989d8157bc723ce40af7f482d634614c56
Jordan <jordan@hrvy.uk>
2025-03-20 13:20:02 +0000

    fix the schema validation for update_interval and timeout
    allows both float or int

--- pyfuelprices/schemas.py
+++ pyfuelprices/schemas.py
@@ -17,8 +17,8 @@ BASE_CONFIG_SCHEMA = vol.Schema(
         vol.Required("providers",default={}): SOURCE_BASE_CONFIG,
         vol.Required("areas", default=[]): list[AREA_CONFIG],
         vol.Optional("country_code", default=""): str,
-        vol.Required("update_interval", default=24): int,
-        vol.Required("timeout", default=30): int
+        vol.Required("update_interval", default=24): vol.Any(float, int),
+        vol.Required("timeout", default=30): vol.Any(float, int)
     },
     extra=vol.ALLOW_EXTRA
 )
